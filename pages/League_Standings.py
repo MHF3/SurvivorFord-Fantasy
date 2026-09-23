2490020d91e9f0a42b17ed6567d1b92436d674aa
MHF3 <86084247+MHF3@users.noreply.github.com>
2026-09-22 20:31:43 -0400

    Cut team number API calls to one read operation

--- pages/League_Standings.py
+++ pages/League_Standings.py
@@ -13,8 +13,8 @@ if ('sheet' not in st.session_state):
     st.session_state.sheet = bot.open_by_url(st.secrets['spreadsheet'])
 
 if ('team_scores' not in st.session_state):
-    teams = st.session_state.sheet.worksheet('Team Information')
-    team_scores = {teams.cell(row, 2).value: teams.cell(row, 4).numeric_value for row in range(2, teams.row_count + 1)}
+    teams_data = st.session_state.sheet.worksheet('Team Information').get_all_values()
+    team_scores = {teams_data[row, 1]: int(teams_data[row, 3]) for row in range(1, len(teams_data))}
     st.session_state.team_scores = {team: score for team, score in sorted(team_scores.items(), key = lambda item: item[1], reverse = True)}
 
 st.header('Standings')
